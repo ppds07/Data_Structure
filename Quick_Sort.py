56e1e0a83ca0f6cf74276fee6f507cdc15fea2f5
Priyadharshan Sharma <74788555+ppds07@users.noreply.github.com>
2022-06-03 11:23:57 +0530

    Create Quick_Sort.py

---
+++ Quick_Sort.py
@@ -0,0 +1,11 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+arr = [10,30,50,60,20]
+quicksort(arr)
+print("Sorted Elements:",arr)
